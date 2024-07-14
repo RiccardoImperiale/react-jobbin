a1d2e40ee0159e74df9ecd71aa60eaceaf73b72b
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-07-14 15:27:22 +0200

    add proxy to backend

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -69,7 +69,7 @@ const JobPage = ({ deleteJob }) => {
 }
 
 const jobLoader = async ({ params }) => {
-    const res = await fetch(`http://localhost:8000/jobs/${params.id}`)
+    const res = await fetch(`/api/jobs/${params.id}`)
     const data = await res.json()
     return data;
 }
